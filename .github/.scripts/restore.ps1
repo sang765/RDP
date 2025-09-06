e74cf76b249fda72c2b68807891d4607c7a40d44
Hà Tiến Sáng <bemimnitran@gmail.com>
2025-09-06 12:52:34 +0700

    Update .scripts/restore.ps1

--- .github/.scripts/restore.ps1
+++ .github/.scripts/restore.ps1
@@ -1,2 +1,9 @@
-
-
+$backupDir = "C:\backup"
+$userDir = "C:\Users\${{ secrets.VNC_USER || 'vncrunner' }}"
+$folders = @("AppData", "LocalAppData", "Downloads", "Documents")
+foreach ($folder in $folders) {
+    $zipFile = "$backupDir/$folder.zip"
+    if (Test-Path $zipFile) {
+        Expand-Archive -Path $zipFile -DestinationPath "$userDir\$folder" -Force
+    }
+}
