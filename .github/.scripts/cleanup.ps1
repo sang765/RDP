8d8bd8a6cc31678f8d51d7d1385ee97291b207a1
Hà Tiến Sáng <bemimnitran@gmail.com>
2025-09-06 12:53:17 +0700

    Update .scripts/cleanup.ps1

--- .github/.scripts/cleanup.ps1
+++ .github/.scripts/cleanup.ps1
@@ -1,2 +1,7 @@
+# Dừng VNC service
+$vncDir = "C:\tigervnc"
+.$vncDir\vncserver.exe -service -stop
 
-
+# Xóa thư mục tạm
+Remove-Item -Path "C:\backup" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\tigervnc" -Recurse -Force -ErrorAction SilentlyContinue
